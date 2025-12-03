ca85cf8e624026d6885b5cd8bbf878a7b881d8b4
OMGcreative <jeremy@omgcreative.com.au>
2025-12-03 12:21:13 +1100

    Delete extra bak directory

--- bak/popup.js.bak.js
+++
@@ -1,4 +0,0 @@
-// Legacy/no-op popup stub.
-// Functionality has moved to `assets/js/popup.js` which uses data-popup-step attributes.
-// Keep this file as a harmless stub to avoid accidental script loads.
-/* no-op */
